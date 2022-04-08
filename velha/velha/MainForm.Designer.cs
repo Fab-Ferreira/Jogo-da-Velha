5f3e1fdfaf275ee953b75046c8ed9ab7004b908d
Fab-Ferreira <103126317+Fab-Ferreira@users.noreply.github.com>
2022-04-08 08:56:14 -0300

    Add files via upload

---
+++ velha/velha/MainForm.Designer.cs
@@ -0,0 +1,309 @@
+﻿/*
+ * Criado por SharpDevelop.
+ * Usuário: Fab
+ * Data: 05/04/2022
+ * Hora: 17:34
+ * 
+ * Para alterar este modelo use Ferramentas | Opções | Codificação | Editar Cabeçalhos Padrão.
+ */
+namespace velha
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Button button9;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label winsP;
+		private System.Windows.Forms.Label winsB;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label empates;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label rodada;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.button9 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.winsP = new System.Windows.Forms.Label();
+			this.winsB = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.empates = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.rodada = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(192, 9);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(328, 44);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "JOGO DA VELHA";
+			// 
+			// label3
+			// 
+			this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.label3.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(55, 53);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(370, 370);
+			this.label3.TabIndex = 2;
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.LightCyan;
+			this.button1.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(71, 71);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(100, 100);
+			this.button1.TabIndex = 11;
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.ExClick);
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.Color.LightCyan;
+			this.button2.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.Location = new System.Drawing.Point(192, 71);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(100, 100);
+			this.button2.TabIndex = 12;
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.ExClick);
+			// 
+			// button3
+			// 
+			this.button3.BackColor = System.Drawing.Color.LightCyan;
+			this.button3.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button3.Location = new System.Drawing.Point(309, 71);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(100, 100);
+			this.button3.TabIndex = 13;
+			this.button3.UseVisualStyleBackColor = false;
+			this.button3.Click += new System.EventHandler(this.ExClick);
+			// 
+			// button4
+			// 
+			this.button4.BackColor = System.Drawing.Color.LightCyan;
+			this.button4.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button4.Location = new System.Drawing.Point(71, 189);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(100, 100);
+			this.button4.TabIndex = 15;
+			this.button4.UseVisualStyleBackColor = false;
+			this.button4.Click += new System.EventHandler(this.ExClick);
+			// 
+			// button5
+			// 
+			this.button5.BackColor = System.Drawing.Color.LightCyan;
+			this.button5.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button5.Location = new System.Drawing.Point(192, 189);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(100, 100);
+			this.button5.TabIndex = 14;
+			this.button5.UseVisualStyleBackColor = false;
+			this.button5.Click += new System.EventHandler(this.ExClick);
+			// 
+			// button6
+			// 
+			this.button6.BackColor = System.Drawing.Color.LightCyan;
+			this.button6.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button6.Location = new System.Drawing.Point(309, 189);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(100, 100);
+			this.button6.TabIndex = 16;
+			this.button6.UseVisualStyleBackColor = false;
+			this.button6.Click += new System.EventHandler(this.ExClick);
+			// 
+			// button7
+			// 
+			this.button7.BackColor = System.Drawing.Color.LightCyan;
+			this.button7.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button7.Location = new System.Drawing.Point(71, 306);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(100, 100);
+			this.button7.TabIndex = 16;
+			this.button7.UseVisualStyleBackColor = false;
+			this.button7.Click += new System.EventHandler(this.ExClick);
+			// 
+			// button8
+			// 
+			this.button8.BackColor = System.Drawing.Color.LightCyan;
+			this.button8.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button8.Location = new System.Drawing.Point(192, 306);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(100, 100);
+			this.button8.TabIndex = 17;
+			this.button8.UseVisualStyleBackColor = false;
+			this.button8.Click += new System.EventHandler(this.ExClick);
+			// 
+			// button9
+			// 
+			this.button9.BackColor = System.Drawing.Color.LightCyan;
+			this.button9.Font = new System.Drawing.Font("Arial", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button9.Location = new System.Drawing.Point(309, 306);
+			this.button9.Name = "button9";
+			this.button9.Size = new System.Drawing.Size(100, 100);
+			this.button9.TabIndex = 18;
+			this.button9.UseVisualStyleBackColor = false;
+			this.button9.Click += new System.EventHandler(this.ExClick);
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(452, 71);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(218, 23);
+			this.label1.TabIndex = 19;
+			this.label1.Text = "Vitórias do jogador 1:";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(441, 189);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(240, 23);
+			this.label4.TabIndex = 20;
+			this.label4.Text = "Vitórias do jogador 2:";
+			this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// winsP
+			// 
+			this.winsP.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.winsP.Location = new System.Drawing.Point(512, 105);
+			this.winsP.Name = "winsP";
+			this.winsP.Size = new System.Drawing.Size(100, 23);
+			this.winsP.TabIndex = 21;
+			this.winsP.Text = "0";
+			this.winsP.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// winsB
+			// 
+			this.winsB.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.winsB.Location = new System.Drawing.Point(512, 221);
+			this.winsB.Name = "winsB";
+			this.winsB.Size = new System.Drawing.Size(100, 23);
+			this.winsB.TabIndex = 22;
+			this.winsB.Text = "0";
+			this.winsB.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label5
+			// 
+			this.label5.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(441, 289);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(240, 23);
+			this.label5.TabIndex = 23;
+			this.label5.Text = "Empates:";
+			this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// empates
+			// 
+			this.empates.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.empates.Location = new System.Drawing.Point(512, 312);
+			this.empates.Name = "empates";
+			this.empates.Size = new System.Drawing.Size(100, 23);
+			this.empates.TabIndex = 24;
+			this.empates.Text = "0";
+			this.empates.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label6
+			// 
+			this.label6.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label6.Location = new System.Drawing.Point(452, 383);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(94, 23);
+			this.label6.TabIndex = 25;
+			this.label6.Text = "Vez do:";
+			this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// rodada
+			// 
+			this.rodada.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.rodada.ForeColor = System.Drawing.Color.SeaGreen;
+			this.rodada.Location = new System.Drawing.Point(536, 383);
+			this.rodada.Name = "rodada";
+			this.rodada.Size = new System.Drawing.Size(110, 23);
+			this.rodada.TabIndex = 26;
+			this.rodada.Text = "Player 1";
+			this.rodada.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.LightCyan;
+			this.ClientSize = new System.Drawing.Size(682, 452);
+			this.Controls.Add(this.rodada);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.empates);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.winsB);
+			this.Controls.Add(this.winsP);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button9);
+			this.Controls.Add(this.button8);
+			this.Controls.Add(this.button7);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label3);
+			this.Name = "MainForm";
+			this.Text = "velha";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+	}
+}
